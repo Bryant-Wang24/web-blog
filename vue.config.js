a8f7250bcd85f8035933297e9f20d2d4a9acc65e
Bryant-Wang24 <485969746@qq.com>
2023-04-06 11:12:52 +0800

    添加cross-env

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+    
     devServer: {
         proxy: {
             '/api': {
-                target: 'https://120.79.23.205:9000',
+                // target: 'https://120.79.23.205:9000',
+                target:process.env.VUE_APP_ADDRESS,
                 changeOrigin: true,
                 // pathRewrite: {
                 //     '^/api': '/api/v1'
